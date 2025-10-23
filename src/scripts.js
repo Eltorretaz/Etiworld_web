8919f5c29159070e54083061fa21f04a4a728ec6
Eltorretaz <eltorretaz470@gmail.com>
2025-10-23 18:59:42 -0400

    commiteado

--- src/scripts.js
+++ src/scripts.js
@@ -44,3 +44,55 @@ setInterval(() => {
 
 }, INTERVAL_DELAY);
 
+let intervalId = 7000;
+
+function nextVideo(id1, id2, id3) {
+        const video = document.getElementById(id1);
+        const video2 = document.getElementById(id2);
+        const video3 = document.getElementById(id3);
+
+        let count1 = 1;
+        let count2 = 6;
+        let count3 = 11;
+
+        setInterval(() => {
+
+            count1 = count1 + 1;
+            console.log(count1);
+            count2 = count2 + 1;
+            console.log(count2);
+            count3 = count3 + 1;
+            console.log(count3);
+
+            if (count1 > 5) {
+                count1 = 1;
+            }
+            if (count2 > 10) {
+                count2 = 6;
+            }
+            if (count3 > 15) {
+                count3 = 11;
+            }
+
+            let timeout = 1000; // Duración de la transición en milisegundos
+            video.classList.replace('opacity-100', 'opacity-0');
+            video2.classList.replace('opacity-100', 'opacity-0');
+            video3.classList.replace('opacity-100', 'opacity-0');
+
+        setTimeout(() => {
+            video['src'] = `./assets/videos/video${count1}.mp4`;
+            video2['src'] = `./assets/videos/video${count2}.mp4`;
+            video3['src'] = `./assets/videos/video${count3}.mp4`;
+
+            video.classList.replace('opacity-0', 'opacity-100');
+            video2.classList.replace('opacity-0', 'opacity-100');
+            video3.classList.replace('opacity-0', 'opacity-100');
+        }, timeout);
+
+
+    }, intervalId);
+}
+
+nextVideo('video1', 'video2', 'video3');
+
+
